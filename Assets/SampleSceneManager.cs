dd0c2f5cb2cb50a1cfa3674e65b8eef6548c6d4c
Sam Murphy <murphysam1997@gmail.com>
2020-02-02 10:48:46 +1100

    Merge

---
+++ Assets/SampleSceneManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SampleSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SharedData.player1Joined)
+        {
+            Debug.Log("Player 1 Joined");
+        }
+        else
+        {
+            GameObject.Destroy(GameObject.FindWithTag("Player 1").transform.parent.gameObject);
+        }
+
+        if (SharedData.player2Joined)
+        {
+            Debug.Log("Player 2 Joined");
+        }
+        else
+        {
+            GameObject.Destroy(GameObject.FindWithTag("Player 2").transform.parent.gameObject);
+        }
+
+        if (SharedData.player3Joined)
+        {
+            Debug.Log("Player 3 Joined");
+        }
+        else
+        {
+            GameObject.Destroy(GameObject.FindWithTag("Player 3").transform.parent.gameObject);
+        }
+
+        if (SharedData.player4Joined)
+        {
+            Debug.Log("Player 4 Joined");
+        }
+        else
+        {
+            GameObject.Destroy(GameObject.FindWithTag("Player 4").transform.parent.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Submit"))
+        {
+            SharedData.Reset();
+            Debug.Log("Loading StartScreen");
+            SceneManager.LoadScene("StartScreen", LoadSceneMode.Single);
+        }
+    }
+}
---
+++ Assets/StartScreenManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("X P1"))
+        {
+            SharedData.player1Joined = true;
+            // TODO: Update the UI.
+        }
+
+        if (Input.GetButton("X P2"))
+        {
+            SharedData.player2Joined = true;
+            // TODO: Update the UI.
+        }
+
+        if (Input.GetButton("X P3"))
+        {
+            SharedData.player3Joined = true;
+            // TODO: Update the UI.
+        }
+
+        if (Input.GetButton("X P4"))
+        {
+            SharedData.player4Joined = true;
+            // TODO: Update the UI.
+        }
+
+        if (Input.GetButton("Submit"))
+        {
+            if (SharedData.player1Joined || SharedData.player2Joined || SharedData.player3Joined || SharedData.player4Joined)
+            {
+                Debug.Log("Loading SampleScene");
+                SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+            }
+            else
+            {
+                Debug.Log("At least one player has to join the game.");
+            }
+        }
+    }
+}
