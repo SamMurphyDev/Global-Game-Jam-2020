064546f25bbcbf58120ad931d79b251b583acf75
Kyle Neideck <kyle@bearisdriving.com>
2020-02-01 14:40:13 +1100

    Make the character bounce off walls.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 10;
-    public float rotationSpeed = 1;
+    public float rotationSpeed = 100;
+    public float uprightingTorque = 75.0f;
     public Rigidbody rb;
 
-    public Vector3 movemet = new Vector3();
+    public Vector3 movement = new Vector3();
 
     // Start is called before the first frame update 
     void Start()
@@ -19,13 +20,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movemet.y = Input.GetAxisRaw("Horizontal");
-        movemet.z = Input.GetAxisRaw("Vertical");
+        movement.y = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
     }
 
-    void FixedUpdate() 
+    void FixedUpdate()
     {
-        transform.Rotate(new Vector3(0, movemet.y * rotationSpeed * Time.fixedDeltaTime, 0));
-        rb.AddForce(transform.forward * movemet.z * moveSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
+        transform.Rotate(new Vector3(0, movement.y * rotationSpeed * Time.fixedDeltaTime, 0));
+
+        Vector3 forwardForce = transform.forward * movement.z * moveSpeed * Time.fixedDeltaTime;
+        rb.AddForce(forwardForce, ForceMode.Impulse);
+
+        // Keep the character standing upright.
+        var rot = Quaternion.FromToRotation(transform.up, Vector3.up);
+        rb.AddTorque(new Vector3(rot.x, rot.y, rot.z) * uprightingTorque);
     }
 }
