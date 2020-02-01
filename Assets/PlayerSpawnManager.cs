6da5b2fa95d347ecb4f8622b6f035e44c1fb0b52
Sam Murphy <murphysam1997@gmail.com>
2020-02-01 23:11:44 +1100

    Camera locking on level

---
+++ Assets/PlayerSpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnManager : MonoBehaviour
+{
+    private LevelGeneration levelGen;
+
+    void Start() {
+        levelGen = GetComponent<LevelGeneration>();
+    }
+
+    Vector3 findMostOuterPosition(Vector2Int outerDirection) {
+        Vector2Int startingPos = new Vector2Int(outerDirection.x == -1 ? 0 : levelGen.levelXSize - 1, outerDirection.y == -1 ? 0 : levelGen.levelYSize - 1)
+    }
+
+    public void moveToPlayerSpawn(int playerIndex, GameObject player) {
+
+    }
+}
