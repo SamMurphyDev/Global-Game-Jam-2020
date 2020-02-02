df268a9ebb5fd7b39ad3e0ec236a18ab18b3c8a0
Sam Murphy <murphysam1997@gmail.com>
2020-02-02 15:48:43 +1100

    Add Material Setter

---
+++ Assets/Scripts/MaterialSetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSetter : MonoBehaviour
+{
+    public Material player1;
+    public Material player2;
+    public Material player3;
+    public Material player4;
+
+    public void TagSet(string tag) {
+        switch(tag) {
+            case "Player 1":
+                GetComponent<SkinnedMeshRenderer>().materials.SetValue(player1, 0);
+                break;
+            case "Player 2":
+                GetComponent<SkinnedMeshRenderer>().materials.SetValue(player2, 0);
+                break;
+            case "Player 3":
+                GetComponent<SkinnedMeshRenderer>().materials.SetValue(player3, 0);
+                break;
+            case "Player 4":
+                GetComponent<SkinnedMeshRenderer>().materials.SetValue(player4, 0);
+                break;
+        }
+    }
+}
